d31ee7073c6555b501bcd777b0d634efe3990d35
jquick5 <jay.quick79@gmail.com>
2021-06-08 11:53:59 -0700

    Preparing for deployment

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import { getPost } from '../../actions/postAction';
+import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+    //eslint-disable-next-line
+  }, []);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Bact to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment, index) => (
+          <CommentItem key={index} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alertReducer';
 import auth from './authReducer';
 import profile from './profileReducer';
+import post from './postsReducer';
 
 export default combineReducers({
   alert,
   auth,
   profile,
+  post,
 });
